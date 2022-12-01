07ffd64ff6fe6d40bc3f22229d1bd9cbc6f12324
garbaczmaciej <furn20102@gmail.com>
2022-12-01 09:49:09 +0100

    Day 1 done

---
+++ 1/solve.py
@@ -0,0 +1,10 @@
+
+with open("input.txt", "r") as f:
+    inventories = [[int(calorie) for calorie in inventory.split("\n") if calorie.split()] for inventory in f.read().split("\n\n") if inventory.split()]
+
+calories_total = [sum(inventory) for inventory in inventories]
+print(max(calories_total))
+
+# Part 2
+calories_total.sort()
+print(sum(calories_total[-3:]))
