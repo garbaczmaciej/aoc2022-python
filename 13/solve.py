257b071c75e084a8ad169253d2d21a6d86ea1d0b
garbaczmaciej <furn20102@gmail.com>
2022-12-18 17:52:23 +0100

    Day 13 part 2 done

--- 13/solve.py
+++ 13/solve.py
@@ -2,6 +2,10 @@
 from sys import argv
 
 from copy import deepcopy
+from functools import lru_cache
+
+import numpy as np
+
 
 LEGAL_CHARACTERS = set([str(i) for i in range(0, 10)] + list("[], \n"))
 
@@ -14,7 +18,6 @@ def get_input(filename: str) -> list:
         _in = [[eval(line) for line in pair.split("\n") if line.split()] for pair in content.split("\n\n") if pair.split()]
 
     return _in
-
 
 def compare_inputs(left_input: list, right_input: list) -> int:
     """
@@ -55,11 +58,31 @@ def part1(_in: list) -> None:
         if result == 1:
             _sum += i + 1
 
-    print(_sum)
+    print("PART 1:", _sum)
 
 
 def part2(_in: list) -> None:
-    pass
+    _in = deepcopy(_in)
+    _in = [_input for pair in _in for _input in pair]
+    _in.append([[2]])
+    _in.append([[6]])
+
+    bigger_inputs = dict()
+    for in1 in _in:
+        bigger_inputs[str(in1)] = []
+        for in2 in _in:
+            if compare_inputs(in1, in2) == 1:
+                bigger_inputs[str(in1)].append(in2)
+
+
+    _inputs_ranking = [(key, len(value)) for key, value in bigger_inputs.items()]
+    _inputs_ranking.sort(key=lambda x: x[1], reverse=True)
+    _inputs_sorted = list(map(lambda x: x[0], _inputs_ranking))
+    # print("\n".join(_inputs_sorted))
+
+    solution = (_inputs_sorted.index("[[2]]") + 1) * (_inputs_sorted.index("[[6]]") + 1)
+
+    print("PART 2:", solution)
 
 
 def main() -> None:
